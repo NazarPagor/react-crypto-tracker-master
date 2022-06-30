eb9d64f53fedcf1c8177e179b85b39d886ed5929
Назар <pagor.n2003@gmail.com>
2022-06-30 14:28:57 +0300

    change currency

--- src/CryptoContext.js
+++ src/CryptoContext.js
@@ -3,11 +3,11 @@ import React, { createContext, useContext, useEffect, useState } from "react";
 const Crypto = createContext();
 
 const CryptoContext = ({ children }) => {
-  const [currency, setCurrency] = useState("INR");
-  const [symbol, setSymbol] = useState("₹");
+  const [currency, setCurrency] = useState("UAH");
+  const [symbol, setSymbol] = useState("₴");
 
   useEffect(() => {
-    if (currency === "INR") setSymbol("₹");
+    if (currency === "UAH") setSymbol("₴");
     else if (currency === "USD") setSymbol("$");
   }, [currency]);
 
